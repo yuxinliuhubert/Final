a00fd1f419688c5cd0250b9145e3140618421d73
yuxinliuhubert <yuxinliuhubert@gmail.com>
2021-11-30 00:06:07 -0800

    1

--- final.py
+++ final.py
@@ -408,14 +408,14 @@ try:
                         if alarm_sent_check == 0:
                             if gps.has_fix:
                                 testMessage = "Your friend might have taken a fall on {}/{}/{} at {:02}:{:02}:{:02} UTC".format(gps.timestamp_utc[1],gps.timestamp_utc[2],gps.timestamp_utc[0],gps.timestamp_utc[3],gps.timestamp_utc[4],gps.timestamp_utc[5])
-                                testMessage = "{} at these location coordinates: \n\n {} \n\n**Paste it in Google or Apple Map will give you the street-specific location!** Please contact your friend to confirm safety!".format(testMessage,location_save)
+                                testMessage = "{} at these location coordinates: \n\n {} \n\n**Paste the coordinates in Google or Apple Map will give you the street-specific location!** Please contact your friend to confirm safety!".format(testMessage,location_save)
                                 # print("fix fall message, ",testMessage)
                             else:
                                 last_print = time.gmtime()
                                 print(last_print)
                                 # last_print = gm_time_processor(last_print)
                                 testMessage ="Your friend might have taken a fall on {}/{}/{} at {:02}:{:02}:{:02} UTC".format(last_print[1],last_print[2],last_print[0],last_print[3],last_print[4],last_print[5])
-                                testMessage = "{}. The last active location coordinates were: \n\n {} \n\n**Paste it in Google or Apple Map will give you the street-specific location!** Please contact your friend to confirm safety!".format(testMessage,location_save)
+                                testMessage = "{}. The last active location coordinates were: \n\n {} \n\n**Paste the coordinates in Google or Apple Map will give you the street-specific location!** Please contact your friend to confirm safety!".format(testMessage,location_save)
                                 print("no fix fall message: ",testMessage)
 
 
